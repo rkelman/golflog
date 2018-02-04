e5dbf851524a306f7ee872a8757812003188eebd
Robb Kelman <robb.kelman@gmail.com>
2018-02-04 23:35:28 +0000

    added config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "daxhundc_misc");
+define("DB_PASSWORD", "m1sc-D4x");
+define("DB_DATABASE", "daxhundc_misc");
+
+//$servername = "localhost";
+//$user = "daxhundc_misc";
+//$passwd = "m1sc-D4x";
+//$dbname = "daxhundc_misc";
+
+?>
--- connection.php
+++ connection.php
@@ -10,4 +10,31 @@ function connectDB() {
   $dh_conn = new mysqli($servername, $user, $passwd, $dbname);
   return $dh_conn;
 }
+
+    /**
+    * Get user by email and password
+    */
+   public function getUserByEmailAndPassword($email, $password) {
+
+       $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+
+       $stmt->bind_param("s", $email);
+
+       if ($stmt->execute()) {
+           $user = $stmt->get_result()->fetch_assoc();
+           $stmt->close();
+
+           // verifying user password
+           $salt = $user['salt'];
+           $encrypted_password = $user['encrypted_password'];
+           $hash = $this->checkhashSSHA($salt, $password);
+           // check for password equality
+           if ($encrypted_password == $hash) {
+               // user authentication details are correct
+               return $user;
+           }
+       } else {
+           return NULL;
+       }
+   }
 ?>
