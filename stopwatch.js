bf53248ed9d767124e88e68b702b5488e6f2291e
Robb Kelman <robb.kelman@gmail.com>
2018-02-07 22:47:43 +0000

    updated format in stopwatch.js changed from 1000 of sec to 10 of sec

--- stopwatch.js
+++ stopwatch.js
@@ -12,7 +12,7 @@ angular.module('stopWatchApp', [])
             var mins = parseInt(elapsed / 60000,10);
             elapsed %= 60000;
             var secs = parseInt(elapsed / 1000,10);
-            var ms = parseInt(elapsed/100,10);
+            var ms = parseInt((elapsed/100)%10);
 
             return hours + ':' + mins + ':' + secs + '.' + ms;
         }
