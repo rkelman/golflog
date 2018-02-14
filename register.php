58a1c0dab229737ec0d03234d64622a68fd12e82
Robb Kelman <robb.kelman@gmail.com>
2018-02-14 05:02:52 +0000

    updated register with more FirstName LastName

--- register.php
+++ register.php
@@ -14,14 +14,15 @@ $json_obj = json_decode($json_str, TRUE);
 //print_r($json_obj);
 //print $json_obj->{'name'};
 
-if (isset($json_obj['name']) && isset($json_obj['email']) && isset($json_obj['password'])) {
+if (isset($json_obj['firstname']) && $json_obj[0]['lastname']) && isset($json_obj['email']) && isset($json_obj['password'])) {
 
     // receiving the post params
-    $name = $json_obj['name'];
+    $firstname = $json_obj['firstname'];
+    $lastname = $json_obj['lastname'];
     $email = $json_obj['email'];
     $password = $json_obj['password'];
 
-    echo $name." ".$email." ".$password;
+    //echo $name." ".$email." ".$password;
 
     // check if user is already existed with the same email
     if (isUserRegistered($email)) {
@@ -30,7 +31,7 @@ if (isset($json_obj['name']) && isset($json_obj['email']) && isset($json_obj['pa
         $response["error_msg"] = "User already existed with ".$email;
         echo json_encode($response);
     } else {
-        $user = $db->storeUser($name, $email, $password);
+        $user = storeUser($firstname, $lastname, $email, $password);
         if ($user) {
             // user stored successfully
             $response["error"] = FALSE;
@@ -44,7 +45,8 @@ if (isset($json_obj['name']) && isset($json_obj['email']) && isset($json_obj['pa
             // user failed to store
             $response["error"] = TRUE;
             $response["error_msg"] = "Unknown error occurred in registration";
-            $response["name"]=$name;
+            $response["firstname"]=$firstname;
+            $response["lastname"]=$lastname;
             $response["email"]=$email;
             echo json_encode($response);
         }
@@ -52,7 +54,8 @@ if (isset($json_obj['name']) && isset($json_obj['email']) && isset($json_obj['pa
 } else {
     $response["error"] = TRUE;
     $response["error_msg"] = "Required parameters (name, email or password) is missing";
-    $response["name"]=$json_obj['name'];
+    $response["firstname"]=$json_obj['firstname'];
+    $response["lastname"]=$json_obj['lastname'];
     $response["email"]=$json_obj['email'];
     $response["password"]=$json_obj['password'];
     echo json_encode($response);
