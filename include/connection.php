d1077800862e936ae9eeb0a8e482bca8e3f1f025
rkelman <rkelman@localhost.localdomain>
2018-02-23 22:48:34 -0800

    updated .gitignore for test transfer and removed deprecated DB_Connect

--- include/connection.php
+++ include/connection.php
@@ -3,9 +3,9 @@
 function connectDB() {
 
   $servername = "localhost";
-  $user = "db_user";
-  $passwd = "db_pass";
-  $dbname = "db_name";
+  $user = "daxhundc_misc";
+  $passwd = "m1sc-D4x";
+  $dbname = "daxhundc_misc";
 
   $dh_conn = new mysqli($servername, $user, $passwd, $dbname);
   return $dh_conn;
