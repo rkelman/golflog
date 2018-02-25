ba8262a782d24909241399202051de4bb2e12f18
rkelman <rkelman@localhost.localdomain>
2018-02-24 16:56:50 -0800

    added angular dependencies

--- include/connection.php
+++ include/connection.php
@@ -4,7 +4,7 @@ function connectDB() {
 
   $servername = "localhost";
   $user = "daxhundc_misc";
-  $passwd = "m1sc-D4x";
+  $passwd = "d4x-M1sc";
   $dbname = "daxhundc_misc";
 
   $dh_conn = new mysqli($servername, $user, $passwd, $dbname);
