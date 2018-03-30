9be627c3ec5eba04c791c86e5f9006f25cfde2ba
Robb Kelman <robb.kelman@gmail.com>
2018-03-30 10:22:53 -0700

    troubleshooting list get action on activity

--- activity.php
+++ activity.php
@@ -56,7 +56,7 @@ if (($method != 'POST') && ($method != 'GET')) {
     echo json_encode($response);
   } else {
     $uid=$_GET['uid'];
-    
+
     if ($_GET['type'] == 'summary') {
       $response = getActivitySummary($uid);
       
@@ -65,7 +65,8 @@ if (($method != 'POST') && ($method != 'GET')) {
         http_response_code(400);
       }
       echo json_encode($response, JSON_PRETTY_PRINT);
-    } elseif (/*($_GET['type'] == 'list') ||*/ (isempty($_GET['type'])) || (!isset($_GET['type']))) {
+    //} elseif (/*($_GET['type'] == 'list') ||*/ (isempty($_GET['type'])) || (!isset($_GET['type']))) {
+    } else {
       echo " got here 2";
       if (isset($_GET['number'])) {
         $number = $_GET['number'];
