cd88364ae6ad200377dff85aee835c3277587361
Robb Kelman <robb.kelman@gmail.com>
2018-03-30 10:26:34 -0700

    experimenting summary get action on activity

--- include/functions.php
+++ include/functions.php
@@ -77,7 +77,7 @@ include_once('connection.php');
       if ($getActSumm) {
         $result["success"]=TRUE;
         while ($row = $getActSumm->fetch_assoc()) {
-          $result["success"][] = array(
+          $result[$row['practiceType']] = array(
             'type' => $row['practiceType'],
             'elapsedTime' => $row['sum_time'],
             'count' => $row['count_type']
