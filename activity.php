f9eebe8498b85af89f44255fb2afa4b0a3418359
Robb Kelman <robb.kelman@gmail.com>
2018-03-29 21:05:33 -0700

    cleaned up getActivitySum

--- activity.php
+++ activity.php
@@ -61,7 +61,7 @@ if (($method != 'POST') && ($method != 'GET')) {
       if ($response["success"] == FALSE) {
         http_response_code(400);
       }
-      echo json_encode($response, JSON_FORCE_OBJECT);
+      echo json_encode($response, JSON_PRETTY_PRINT);
     } elseif (($type == 'list') || (isempty($_GET['type'])) || (!isset($_GET['type']))) {
       if (isset($_GET['number'])) {
         $number = $_GET['number'];
