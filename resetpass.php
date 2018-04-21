15bb4643206ff1b0c18cb3b370453a5a86942db6
Robb Kelman <robb.kelman@gmail.com>
2018-04-21 16:48:37 -0700

    resetpass do over

--- resetpass.php
+++ resetpass.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "reset pass works";
+if ((isempty($_POST['password'])) || (isempty($_POST['password_conf']))) {
+  //if either password was sent over null return to reset
+  header('Location: reset.php?mail='.$_POST['mail'].'&key='.$_POST['key'].'&err=passNull');
+} else {
+  echo "reset pass works";
+}
 
 ?>
