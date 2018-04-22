b5cbd58568ecb5a9ffd1e2648621d8945e2fa711
Robb Kelman <robb.kelman@gmail.com>
2018-04-21 17:22:28 -0700

    added check for password mismatch

--- resetpass.php
+++ resetpass.php
@@ -4,7 +4,11 @@ if ((empty($_POST['password'])) || (empty($_POST['password_conf']))) {
   //if either password was sent over null return to reset
   header('Location: resetform.php?mail='.$_POST['mail'].'&key='.$_POST['key'].'&err=passNull');
 } else {
-  echo "reset pass works";
+  if ($pass1 == $pass2) {
+    echo "reset pass works";
+  } else {
+    header('Location: resetform.php?mail='.$_POST['mail'].'&key='.$_POST['key'].'&err=passMismatch');
+  }
 }
 
 ?>
