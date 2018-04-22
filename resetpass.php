d25eee767480a2b0acad3aafd6192fb503f1626f
Robb Kelman <robb.kelman@gmail.com>
2018-04-21 17:06:15 -0700

    resetpass start over

--- resetpass.php
+++ resetpass.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isempty($_POST['password']))/* || (isempty($_POST['password_conf']))*/ {
+if (empty($_POST['password']))/* || (isempty($_POST['password_conf']))*/ {
   //if either password was sent over null return to reset
   header('Location: reset.php?mail='.$_POST['mail'].'&key='.$_POST['key'].'&err=passNull');
 } else {
