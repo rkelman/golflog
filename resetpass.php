e6421a0ab66a171f112174f084cbf80266123e7e
Robb Kelman <robb.kelman@gmail.com>
2018-04-21 17:11:55 -0700

    adding check for null password_conf

--- resetpass.php
+++ resetpass.php
@@ -1,6 +1,6 @@
 <?php
 
-if (empty($_POST['password']))/* || (isempty($_POST['password_conf']))*/ {
+if ((empty($_POST['password'])) || (empty($_POST['password_conf']))) {
   //if either password was sent over null return to reset
   header('Location: resetform.php?mail='.$_POST['mail'].'&key='.$_POST['key'].'&err=passNull');
 } else {
