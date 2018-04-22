b1197d0ac971912f50637326b81cbad47d1d5ff5
Robb Kelman <robb.kelman@gmail.com>
2018-04-21 17:08:54 -0700

    updated reset to forward to resetform on null password

--- resetpass.php
+++ resetpass.php
@@ -2,7 +2,7 @@
 
 if (empty($_POST['password']))/* || (isempty($_POST['password_conf']))*/ {
   //if either password was sent over null return to reset
-  header('Location: reset.php?mail='.$_POST['mail'].'&key='.$_POST['key'].'&err=passNull');
+  header('Location: resetform.php?mail='.$_POST['mail'].'&key='.$_POST['key'].'&err=passNull');
 } else {
   echo "reset pass works";
 }
